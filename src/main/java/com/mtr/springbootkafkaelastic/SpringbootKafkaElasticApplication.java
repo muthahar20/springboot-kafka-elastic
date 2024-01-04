78ec62e1339bfea17d3e8b27bc80e47178f3ad64
muthahar20@gmail.com <muthahar20@gmail.com>
2024-01-04 10:06:46 -0500

    initial commit

---
+++ src/main/java/com/mtr/springbootkafkaelastic/SpringbootKafkaElasticApplication.java
@@ -0,0 +1,13 @@
+package com.mtr.springbootkafkaelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaElasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtr/springbootkafkaelastic/SpringbootKafkaElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtr.springbootkafkaelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaElasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
